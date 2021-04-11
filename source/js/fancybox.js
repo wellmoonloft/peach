efc57dded0419e100ce4fc13240b9ca53f716a76
wellmoonloft <wellmoonloft@gmail.com>
2021-04-11 12:38:59 +0800

    Create fancybox.js

---
+++ source/js/fancybox.js
@@ -0,0 +1,24 @@
+  // fancybox js
+  let fancyTimer = setInterval(function(){
+  if(!window.$){
+    return;
+  }
+  $(document).ready(function() {
+    $(".markdown-body img").each(function () {
+      if($(this).parent().get(0).tagName.toLowerCase() === "a") {
+        return;
+      }
+      var element = document.createElement("a");
+      $(element).attr("data-fancybox", "gallery");
+      $(element).attr("style", "text-decoration: none; outline: none; border: 0px none transparent;");
+      if ($(this).attr("data-original")) {
+        $(element).attr("href", $(this).attr("data-original"));
+      } else {
+        $(element).attr("href", $(this).attr("src"));
+      }
+      $(this).wrap(element);
+    });
+
+    clearInterval(fancyTimer);
+  });
+  }, 10); 
